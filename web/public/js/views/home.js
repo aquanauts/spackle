0fd2bbba632f06147d299107adcffbd7f40dcbce
Lindsey Shavers <linshavers@gmail.com>
2020-06-30 13:22:05 -0500

    Fix query when version is not submitted

--- web/public/js/views/home.js
+++ web/public/js/views/home.js
@@ -5,10 +5,7 @@ export default function(packageArg) {
     view.find('.searchForm').submit(onSubmit);
     view.find('.formSubmitButton').click(onSubmit);
 
-    if (packageArg) {
-        queryPackage(packageArg);
-    }
-
+    // handle autocomplete
     $.get("/project_names").then(function(data) {
         let listOfProjectNames = data.projects;
         listOfProjectNames.sort();
@@ -17,21 +14,22 @@ export default function(packageArg) {
         }));
     });
 
+    if (packageArg) {
+        queryPackage(packageArg);
+    }
 
     function queryPackage(packageArg) {
         // show loading message
         view.find('.loading').css("display", "block");
         // clear old results 
         view.find('.table_project').remove();
-        // get list from /project
+        // get package list
         const args = packageArg.split('=');
         if (args.length > 1) {
-            getPackageListFromProjectWithVersion(args[0], args[1]);
-        } else if (/\d/.test(packageArg)) {
-            getPackageListForDependency(args[0]);
+            getPackageListWithVersion(args[0], args[1]);
         } else {
-            getPackageListFromProject(args[0]);
-        }
+            getPackageList(args[0]);
+        } 
     }
 
     function onSubmit(){
@@ -82,55 +80,45 @@ export default function(packageArg) {
               "id": "table",
               html: table.join( "" )
           }).appendTo(view.find('.mx-auto'))).then(function() {
-              // hide loading messag
+              // hide loading message
               view.find('.loading').css("display", "none");
           });
         return false;
     }
 
-    // getting packagelist from /project
-    function getPackageListFromProject(response){
-        const table = ["<h2>", response, " Packages: </h2>"] 
-        table.push("<tr>",
-            "<th>Package Name</th>",
-            "<th>Version</th>",
-            "<th>Build</th>",
-            "<th>Channel</th>",
-            "<th>Architecture</th>",
-            "<th>Size</th>",
-            "<th>Dependencies</th>",
-            "</tr>")
-        $.get("/project?project_name="+response).then(function(data) {
-            const output = data.packages;
-            loadResponseTable(output, table);
-        }); 
-        return false;
-    }
-
-    function getPackageListForDependency(version) {
+    // getting package list 
+    function getPackageList(version) {
         let decodedVersion = decodeURIComponent(version);
         let nameVersSplit = decodedVersion.split(" ");
         let name = nameVersSplit[0];
         let vers = nameVersSplit[1];
-        const table = ["<h2>", name, " ", vers + " Packages: </h2>"];
-        table.push("<tr>",
-            "<th>Package Name</th>",
-            "<th>Version</th>",
-            "<th>Build</th>",
-            "<th>Channel</th>",
-            "<th>Architecture</th>",
-            "<th>Size</th>",
-            "<th>Dependencies</th>",
-            "</tr>")
-        $.get("/version?project_name="+name+"&version="+vers).then(function(data) {
+        let queryString
+        let table
+        if (vers == undefined) {
+            queryString = "/project?project_name="+name
+            table = ["<h2>" + name + " " + " Packages: </h2>"];
+        } else {
+            queryString = "/version?project_name="+name+"&version="+vers
+            table = ["<h2>" + name + " " + vers + " Packages: </h2>"];
+        }
+        $.get(queryString).then(function(data) {
             const output = data.packages;
+            table.push("<tr>",
+                "<th>Package Name</th>",
+                "<th>Version</th>",
+                "<th>Build</th>",
+                "<th>Channel</th>",
+                "<th>Architecture</th>",
+                "<th>Size</th>",
+                "<th>Dependencies</th>",
+                "</tr>")
             loadResponseTable(output, table);
         }); 
         return false;
     }
 
-    // getting packagelist from /version
-    function getPackageListFromProjectWithVersion(name, version){
+    // getting package list with version
+    function getPackageListWithVersion(name, version){
         const table = ["<h2>", name, "-", decodeURIComponent(version) + " Packages: </h2>"];
         table.push("<tr>",
             "<th>Package Name</th>",
